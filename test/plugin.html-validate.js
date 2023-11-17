0416ae9ae6110edd468ac26aed5049d99d51b50d
Raza403 <raza403@gmail.com>
2023-11-17 17:47:57 +0500

    github-pages-template added canonical test. This PR is made for #5497

--- test/plugin.html-validate.js
+++ test/plugin.html-validate.js
@@ -10,6 +10,7 @@ const { definePlugin } = require("html-validate");
 const { rules: mailtoAwesomeRules } = require("./plugin.html-validate.mailto-awesome.js");
 const { rules: externalLinksRules } = require("./plugin.html-validate.external-links.js");
 const { rules: noJqueryRules } = require("./plugin.html-validate.no-jquery.js");
+const { rules: ValidateCanonicalRules } = require("./plugin.html-validate.canonical.js");
 const { rules: latestPackagesRules } = require("./plugin.html-validate.latest-packages.js");
 
 module.exports = definePlugin({
@@ -18,6 +19,7 @@ module.exports = definePlugin({
     ...mailtoAwesomeRules,
     ...externalLinksRules,
     ...noJqueryRules,
+    ...ValidateCanonicalRules,
     ...latestPackagesRules,
   },
   configs: {
@@ -26,6 +28,7 @@ module.exports = definePlugin({
         "pacific-medical-training/mailto-awesome": "error",
         "pacific-medical-training/external-links": "error",
         "pacific-medical-training/no-jquery": "error",
+        "pacific-medical-training/canonical": "error",
         "pacific-medical-training/latest-packages": "error",
       },
     },
