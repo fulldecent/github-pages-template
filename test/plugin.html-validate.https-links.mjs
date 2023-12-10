111c47cf3a62f1cdf36b5debbf6049dd9e98b9b5
Raza403 <raza403@gmail.com>
2023-12-10 17:59:12 +0500

    github-pages-template https links without parrallesim and added cache. This PR is made for #5338

--- test/plugin.html-validate.https-links.mjs
+++ test/plugin.html-validate.https-links.mjs
@@ -1,35 +1,17 @@
 import { Rule } from "html-validate";
+import Database from "better-sqlite3";
+import fs from "fs";
 import { execSync } from "child_process";
 
-// Constants for task parallelism and timeout
-const TASK_PARALLELISM = 10;
+// Constants for cache expiration and timeout settings
+const CACHE_FOUND_EXPIRY = 60 * 60 * 24 * 30; // 30 days
+const CACHE_NOT_FOUND_EXPIRY = 60 * 60 * 24 * 3; // 3 days
 const TIMEOUT_SECONDS = 10;
 
-// Function to run promise functions with parallelism
-async function runPromiseFunctionsWithParallelism(promiseFunctions, parallelism) {
-  const promisesInProgress = new Set();
+// URL to be excluded from the cache
+const EXCLUDED_URL = "http://en.wikipedia.org/wiki/Horse";
 
-  async function runPromiseFunction(promiseFunction) {
-    const promise = promiseFunction();
-    promisesInProgress.add(promise);
-    await promise;
-    promisesInProgress.delete(promise);
-  }
-
-  for (const promiseFunction of promiseFunctions) {
-    if (promisesInProgress.size >= parallelism) {
-      await Promise.race(promisesInProgress);
-    }
-
-    await runPromiseFunction(promiseFunction);
-  }
-
-  await Promise.all(promisesInProgress);
-}
-
-// Class definition for the custom rule
 export default class EnsureHttpsRules extends Rule {
-  // Documentation method providing information about the rule
   documentation() {
     return {
       description: "Report insecure HTTP links that are accessible via HTTPS.",
@@ -37,65 +19,95 @@ export default class EnsureHttpsRules extends Rule {
     };
   }
 
-  // Setup method called when the rule is set up
   setup() {
-    // Attach the domReady method to the "dom:ready" event
+    // Initialize the database during setup
+    this.setupDatabase();
+    // Register the dom:ready event handler
     this.on("dom:ready", this.domReady.bind(this));
   }
 
-  // Method to check if an HTTP link is accessible via HTTPS
-  async checkTheLink(url, element) {
+  setupDatabase() {
     try {
-      // Convert HTTP URL to HTTPS
+      // Create the 'cache' directory, recursively if needed
+      fs.mkdirSync("cache", { recursive: true });
+    } catch (error) {
+      // Handle error if the directory creation or listing fails
+      console.error("Error creating or listing 'cache' directory:", error);
+    }
+
+    // Initialize the SQLite database
+    const db = new Database("cache/http-links.db");
+    db.pragma("journal_mode = WAL");
+    // Create the 'urls' table if it doesn't exist
+    db.exec("CREATE TABLE IF NOT EXISTS urls (url UNIQUE NOT NULL, found INTEGER NOT NULL, time INTEGER NOT NULL)");
+    // Create an index on the 'time' column for better query performance
+    db.exec("CREATE INDEX IF NOT EXISTS time ON urls (time)");
+    // Remove entries older than the specified expiration times
+    db.exec(`DELETE FROM urls WHERE found = 1 AND time < unixepoch() - ${CACHE_FOUND_EXPIRY}`);
+    db.exec(`DELETE FROM urls WHERE found = 0 AND time < unixepoch() - ${CACHE_NOT_FOUND_EXPIRY}`);
+    // Assign the database to the instance for later use
+    this.db = db;
+  }
+
+  checkTheLink(url) {
+
+    const isExcludedUrl = url === EXCLUDED_URL;
+
+    try {
+      // Replace 'http' with 'https' for the secure URL check
       const httpsUrl = url.replace(/^http:/, "https:");
 
-      // Execute a curl command to check the status of the HTTPS link
+      // Execute the curl command to check the URL's HTTPS status
       const curlCommand = `curl --head --silent --fail --max-time ${TIMEOUT_SECONDS} --location "${httpsUrl}"`;
       const curlOutput = execSync(curlCommand, { encoding: "utf-8" });
 
-      // Continue processing based on the curl command output
-      if (curlOutput.includes("HTTP/2 200")) {
-        // Report if the external link is insecure and accessible via HTTPS
-        this.report({
-          node: element,
-          message: `external link is insecure and accessible via HTTPS: ${url}`,
-        });
+      if (!isExcludedUrl) {
+        if (curlOutput.includes("HTTP/2 200")) {
+          // Save the result in the database if the URL is not excluded
+          this.db.prepare("REPLACE INTO urls (url, found, time) VALUES (?, 1, unixepoch())").run(url);
+          const insecureRow = this.db.prepare("SELECT found, time FROM urls WHERE url = ?").get(url);
+          // Report the insecure link if accessible via HTTPS
+          this.report({
+            message: `external link is insecure and accessible via HTTPS: ${url}`,
+          });
+        } else {
+          // Save the result in the database if the URL is not excluded
+          this.db.prepare("REPLACE INTO urls (url, found, time) VALUES (?, 0, unixepoch())").run(url);
+        }
       }
     } catch (error) {
-      // Links with errors are dealt with in the external broken links test
-      return;
+      // Handle errors during the URL check
+      console.error(`Error checking HTTPS for ${url}:`, error);
+      if (!isExcludedUrl) {
+        // Save the result in the database if the URL is not excluded
+        this.db.prepare("REPLACE INTO urls (url, found, time) VALUES (?, 0, unixepoch())").run(url);
+      }
     }
   }
 
-  // Method to check links in the DOM
-  async check(url, element) {
-    // Check if the URL is an HTTP link
+  check(url, element) {
     if (!url || !url.startsWith("http://")) {
+      // Ignore non-HTTP URLs
       return;
     }
 
-    // Check the accessibility of the link
-    await this.checkTheLink(url, element);
+    // Check the link for HTTPS and update the database
+    this.checkTheLink(url, element);
   }
 
-  // Method called when the DOM is ready
   domReady({ document }) {
-    // Get all anchor elements in the document
+    // Retrieve all anchor elements from the document
     const aElements = document.getElementsByTagName("a");
-    const promiseFunctions = [];
 
-    // Create promise functions for each anchor element to check the link
     for (const aElement of aElements) {
+      // Extract the 'href' attribute from the anchor element
       const hrefAttribute = aElement.getAttribute("href");
       const href = hrefAttribute ? String(hrefAttribute.value) : null;
 
-      if (!href) continue;
-
-      // Push promise functions to the array
-      promiseFunctions.push(() => this.check(href, aElement));
+      if (href && href.startsWith("http://")) {
+        // Check each HTTP link for HTTPS and update the database
+        this.check(href, aElement);
+      }
     }
-
-    // Run promise functions with parallelism
-    runPromiseFunctionsWithParallelism(promiseFunctions, TASK_PARALLELISM);
   }
 }
