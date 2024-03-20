1fa8fa698e9ad16257a842709995f2d2559f9eb3
Raza403 <raza403@gmail.com>
2024-03-20 18:10:22 +0500

    github pages template updated curl to fix false broken links. This PR fixes #62

--- test/plugin.html-validate.external-links.mjs
+++ test/plugin.html-validate.external-links.mjs
@@ -73,7 +73,10 @@ export default class ExternalLinksRule extends Rule {
     }
 
     try {
-      const result = execSync(`curl --head --silent --fail --max-time ${TIMEOUT_SECONDS} --location "${url}"`);
+      const result = execSync(`curl --head --silent --fail --max-time ${TIMEOUT_SECONDS} --location \
+      --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.999 Safari/537.36" \
+      --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" \
+      "${url}"`);
       // Success: link is good
       this.db.prepare("REPLACE INTO urls (url, found, time) VALUES (?, 1, unixepoch())").run(url);
     } catch (error) {
