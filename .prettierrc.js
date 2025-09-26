f17043e90134ce4faa5c7c564b418b9a306e6dd5
William Entriken <github.com@phor.net>
2025-09-26 14:25:32 -0400

    remove option that is shadowed by .prettierignore

--- .prettierrc.js
+++ .prettierrc.js
@@ -11,12 +11,6 @@ module.exports = {
       options: {
         parser: "liquid-html",
       },
-    },
-    {
-      files: "*.md", // Disable Prettier for Markdown files because it is incompatible with Kramdown https://github.com/prettier/prettier/issues/3645
-      options: {
-        parser: null,
-      },
-    },
+    }
   ],
 };
